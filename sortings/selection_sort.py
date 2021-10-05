523de0d6b9319676a1524f72ff77f4bef0328254
Prajna Suresh <57291848+prajnasuresh@users.noreply.github.com>
2021-10-05 23:57:43 +0530

    Update selection_sort.py

--- sortings/selection_sort.py
+++ sortings/selection_sort.py
@@ -1,1 +1,19 @@
 #Selection Sort
+
+
+
+def selection_sort(unsorted_array):
+
+ 
+    for i in range(len(unsorted_array)):
+
+        min_index = i
+        for j in range(i+1,len(unsorted_array)):
+            if unsorted_array[min_index]>unsorted_array[j]:
+                min_index = j
+       # Swapping
+            unsorted_array[i],unsorted_array[min_index] = unsorted_array[min_index],unsorted_array[i]
+
+    print(unsorted_array)
+    
+  
