6a082f1c748a7d06248d09a71d725cb4e8da589b
Csehgal27 <76051927+Csehgal27@users.noreply.github.com>
2021-10-06 15:27:39 +0530

    Update linear_search.py

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -1,9 +1,9 @@
 def linear_search(search_list,key):
-    for i in range(0,len(search_list)):
+    for i in range(len(search_list)):
         if search_list[i]==key:
             return i
     return 0
 
-
-
-
+li=[int(x) for x in input().split()]
+k=int(input())
+print(linear_search(li,k))
